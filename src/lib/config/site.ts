88c2ade1ef17d6192c8109dbd3e73aabee84e69a
ISNing <33572057+ISNing@users.noreply.github.com>
2024-02-29 09:02:50 +0800

    blog: Update site configuration

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'isning/blog',
+  subtitle: '',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Just for recording&sharing something useful/useless',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    avatar: '/assets/avatar@512.jpg',
+    name: 'ISNing',
+    status: '🤔',
+    bio: 'BIO\'s here!'
   },
   themeColor: '#3D4451'
 }
